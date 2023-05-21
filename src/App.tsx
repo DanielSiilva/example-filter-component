d3093297d5c2585413c91c1786442b3beca5e5bb
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-05-21 00:44:29 -0300

    Feat: component filter

--- src/App.tsx
+++ src/App.tsx
@@ -11,25 +11,29 @@ interface Product {
 
 //Mock De Dados
 const products: Product[] = [
-  { id: 1, name: 'Product 1', category: 'Category1' },
-  { id: 2, name: 'Product 2', category: 'Category1' },
-  { id: 3, name: 'Product 3', category: 'Category2' },
-  { id: 4, name: 'Product 4', category: 'Category2' },
-  { id: 5, name: 'Product 5', category: 'Category3' },
-  { id: 6, name: 'Product 6', category: 'Category3' },
+  { id: 1, name: 'Product 1', category: 'Camisas' },
+  { id: 2, name: 'Product 2', category: 'Tenis' },
+  { id: 3, name: 'Product 3', category: 'Tenis' },
+  { id: 4, name: 'Product 4', category: 'Tenis' },
+  { id: 5, name: 'Product 5', category: 'Tenis' },
+  { id: 6, name: 'Product 6', category: 'Camisas' },
 ];
 
 export function App (){
+
   const [category, setCategory] = useState<string>('');
 
   return(
     <div>
       <h1>Product Listing</h1>
-      <CategoryButton name="Category1" setCategory={setCategory} />
-      <CategoryButton name="Category2" setCategory={setCategory} />
-      <CategoryButton name="Category3" setCategory={setCategory} />
+        {products.map(category => (
+          <CategoryButton 
+            key={category.id} 
+            name={category.category} 
+            setCategory={setCategory} 
+          />
+        ))}
       <ProductList products={products} category={category} />
     </div>
-    
   )
 }
